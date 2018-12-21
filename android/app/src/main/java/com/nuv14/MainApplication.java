96752744f285a80838738da9a5a4c491e92b0b64
Brandon Robinson <BrandonLRobinson1@gmail.com>
2018-12-21 00:22:42 -0500

    andriod working

--- android/app/src/main/java/com/nuv14/MainApplication.java
+++ android/app/src/main/java/com/nuv14/MainApplication.java
@@ -1,13 +1,64 @@
+// package com.nuv14;
+
+// import android.app.Application;
+
+// import com.facebook.react.ReactApplication;
+// import com.airbnb.android.react.maps.MapsPackage;
+// import com.facebook.react.ReactNativeHost;
+// import com.facebook.react.ReactPackage;
+// import com.facebook.react.shell.MainReactPackage;
+// import com.facebook.soloader.SoLoader;
+
+// import java.util.Arrays;
+// import java.util.List;
+
+// public class MainApplication extends Application implements ReactApplication {
+
+//   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+//     @Override
+//     public boolean getUseDeveloperSupport() {
+//       return BuildConfig.DEBUG;
+//     }
+
+//     @Override
+//     protected List<ReactPackage> getPackages() {
+//       return Arrays.<ReactPackage>asList(
+//           new MainReactPackage(),
+//             new MapsPackage()
+//       );
+//     }
+
+//     @Override
+//     protected String getJSMainModuleName() {
+//       return "index";
+//     }
+//   };
+
+//   @Override
+//   public ReactNativeHost getReactNativeHost() {
+//     return mReactNativeHost;
+//   }
+
+//   @Override
+//   public void onCreate() {
+//     super.onCreate();
+//     SoLoader.init(this, /* native exopackage */ false);
+//   }
+// }
+
 package com.nuv14;
+
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,11 +71,22 @@ public class MainApplication extends Application implements ReactApplication {
       return BuildConfig.DEBUG;
     }
 
+    // https://github.com/callstack/haul/issues/334
+    // @Override
+    // public void onCreate() {
+    //     super.onCreate();
+    //     DevInternalSettings settings = (DevInternalSettings) getReactGateway().getReactInstanceManager().getDevSupportManager().getDevSettings();
+    //     if (settings != null) {
+    //         settings.setBundleDeltasEnabled(false);
+    //     }
+    // }
+
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage()
+          new MapsPackage(),
+          new RNGooglePlacesPackage()
       );
     }
 
@@ -44,4 +106,5 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
+
 }
